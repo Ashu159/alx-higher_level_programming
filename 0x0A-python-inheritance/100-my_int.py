fbc1e240b01b9340a9f0728d151b1461fc9d37a9
ashenafi <ashenafibezabeh@gmail.com>
2023-07-09 23:55:13 -0700

    inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class MyInt(int):
+    """Definition of class MyInt that inherits from class int"""
+
+    def __eq__(self, other):
+        """Overrides equals, inverting it"""
+        return int(self) != int(other)
+
+    def __ne__(self, other):
+        """Overrides not-equals, inverting it"""
+        return int(self) == int(other)
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_attribute(obj, objname, value):
+    """add attribute to object
+    args:
+        obj: class object
+        objname: object name
+        value: value of attribute
+    """
+    if hasattr(obj, "__dict__") is False:
+        raise TypeError("can't add new attribute")
+    setattr(obj, objname, value)
